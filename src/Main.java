a0272f325b5c42279476acb6615edec9f866751b
andres1770 <andresmoron1770@gmail.com>
2022-12-18 12:13:27 -0500

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("the basic structurte is ready please write down your code in this project");
+
+   int sun=9;
+   int earth = 7;
+   
+
+
     }
 }
